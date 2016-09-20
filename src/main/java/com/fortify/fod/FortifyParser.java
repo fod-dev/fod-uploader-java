287edabbf1d49738ebf348ac49fea9eea8cfd31f
petebeegle <pete.beegle@hpe.com>
2016-09-20 13:21:10 -0400

    sort help command results

--- src/main/java/com/fortify/fod/FortifyParser.java
+++ src/main/java/com/fortify/fod/FortifyParser.java
@@ -2,6 +2,8 @@ package com.fortify.fod;
 
 import org.apache.commons.cli.*;
 
+import java.util.Comparator;
+
 /**
  * Created by petebeegle on 9/20/2016.
  */
@@ -69,10 +71,32 @@ public class FortifyParser {
 
     public void help() {
         HelpFormatter formatter = new HelpFormatter();
+        formatter.setOptionComparator(HelpComparator);
+
         formatter.printHelp( "FodUpload-5.3.jar", options, true );
     }
 
     public Options getOptions() {
         return options;
     }
+
+    /*
+     * Compares options so that they are ordered:
+     * 1.) by required, then by
+     * 2.) short operator
+     * Used for sorting the results of the Help command.
+     */
+    private static Comparator<Option> HelpComparator = new Comparator<Option>() {
+        @Override
+        public int compare(Option o1, Option o2) {
+            String required1 = o1.isRequired() ? "1" : "0";
+            String required2 = o2.isRequired() ? "1" : "0";
+
+            int result = required2.compareTo(required1);
+            if (result == 0) {
+                result = o1.getOpt().compareToIgnoreCase(o2.getOpt());
+            }
+            return result;
+        }
+    };
 }
